9a9dab90a241bcc27fa6308ff9a786dd5580464a
Andre Nascimento <1914andre@gmail.com>
2024-09-23 10:07:15 -0300

    feat: adding files estructuring from cors

--- Library.js
+++ Library.js
@@ -3,6 +3,7 @@ import express from "express"
 import { RoteBoocksG } from "./Src/Rotes/Rotes_Boocks_Guides/Rotes_Guides.js"
 import { FromBoocksCodes } from "./Src/Rotes/Rotes_Boocks_Codes/Rotes_Code.js"
 import { FromBoocksExperiences } from "./Src/Rotes/Rotes_Boocks_Experiences/Rotes_Experiences.js"
+import cors from "cors"
 
 const library = express()
 
@@ -11,6 +12,7 @@ library.use(express.json())
 library.use(RoteBoocksG)
 library.use(FromBoocksCodes)
 library.use(FromBoocksExperiences)
+library.use(cors({origin: 'http://localhost:5173'}))
 
 
 const PORT = 5000
