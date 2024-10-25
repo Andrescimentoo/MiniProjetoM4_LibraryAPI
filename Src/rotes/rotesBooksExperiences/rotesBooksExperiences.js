2d7c27ee11fb1fbb3b33d60a3fa6e0da963ed540
Andre Nascimento <1914andre@gmail.com>
2024-10-25 17:11:01 -0300

    fix: alteration in files to the routes

--- Src/rotes/rotesBooksExperiences/rotesBooksExperiences.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { AllBoocksExperiences } from "../../Controlers/Controller_Boocks_Experiences/Controler_Experiences.js";
-
-const FromBoocksExperiences = Router()
-FromBoocksExperiences.get("/RoteNumberThree",AllBoocksExperiences)
-
-export{FromBoocksExperiences}
