2a564827af5ee9e91397dd5148569c5c6214eced
Andre Nascimento <1914andre@gmail.com>
2024-10-31 16:16:11 -0300

    fix: adding timestamps in model.js

--- Src/models/model.js
+++ Src/models/model.js
@@ -1,82 +1,85 @@
 import { DataTypes} from "sequelize";
 import sequelize from "../dataBases/config.js";
 
-const datacodes = sequelize.define("books_Code",{
+const datacodes = sequelize.define("books_Code", {
     id: {
-        type:DataTypes.INTEGER,
-        primaryKey:true,
-        autoIncrement:true,
-       allowNull:false,
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
     },
-    title:{
-        type:DataTypes.STRING,
-        allowNull:false,
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
     },
-    author:{
-        type:DataTypes.STRING,
-        allowNull:false,
+    author: {
+        type: DataTypes.STRING,
+        allowNull: false,
     },
-    gener:{
-        type:DataTypes.STRING,
-        allowNull:false,
+    gener: {
+        type: DataTypes.STRING,
+        allowNull: false,
     },
-    target_audience:{
-        type:DataTypes.STRING,
-        allowNull:false,
+    target_audience: {
+        type: DataTypes.STRING,
+        allowNull: false,
     },
-    
+}, {
+    timestamps: false //?
 })
 
-const dataExperiences = sequelize.define("books_experiences",{
+const dataExperiences = sequelize.define("books_experiences", {
     id: {
-        type:DataTypes.INTEGER,
-        primaryKey:true,
-        autoIncrement:true,
-        allowNull:false,
-        
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
     },
-    title:{
-        type:DataTypes.STRING,
-        allowNull:false,
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
     },
-    author:{
-        type:DataTypes.STRING,
-        allowNull:false,
+    author: {
+        type: DataTypes.STRING,
+        allowNull: false,
     },
-    gener:{
-        type:DataTypes.STRING,
-        allowNull:false,
+    gener: {
+        type: DataTypes.STRING,
+        allowNull: false,
     },
-    target_audience:{
-        type:DataTypes.STRING,
-        allowNull:false,
+    target_audience: {
+        type: DataTypes.STRING,
+        allowNull: false,
     },
+}, {
+    timestamps: false //?
 })
 
-const dataGuides = sequelize.define("books_guides",{
+const dataGuides = sequelize.define("books_guides", {
     id: {
-        type:DataTypes.INTEGER,
-        primaryKey:true,
-        autoIncrement:true,
-        allowNull:false,
-        
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
     },
-    title:{
-        type:DataTypes.STRING,
-        allowNull:false,
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
     },
-    author:{
-        type:DataTypes.STRING,
-        allowNull:false,
+    author: {
+        type: DataTypes.STRING,
+        allowNull: false,
     },
-    gener:{
-        type:DataTypes.STRING,
-        allowNull:false,
+    gener: {
+        type: DataTypes.STRING,
+        allowNull: false,
     },
-    target_audience:{
-        type:DataTypes.STRING,
-        allowNull:false,
+    target_audience: {
+        type: DataTypes.STRING,
+        allowNull: false,
     },
+}, {
+    timestamps: false //?
 })
 
-export {datacodes,dataExperiences,dataGuides}
+export { datacodes, dataExperiences, dataGuides };
