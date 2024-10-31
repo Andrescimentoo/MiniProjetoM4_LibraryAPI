ae3c67de9fa9d5e2a382b3dc6f52b746ac3f45df
Andre Nascimento <1914andre@gmail.com>
2024-10-31 16:15:20 -0300

    fix:removing the structure of the mock data and adding a new structure of the real data

--- Src/controllers/booksCodes/booksCode.js
+++ Src/controllers/booksCodes/booksCode.js
@@ -1,11 +1,18 @@
 import { datacodes } from "../../models/model.js"
 
-const AllBoocksCodes = (res,req) => {
+const AllBoocksCodes = async (req,res) => {
     try{
-        res.status(200).send(datacodes)
+        const findAllcodes = await datacodes.findAll() //await = espera!...e dps executa
+        res.status(200).send(findAllcodes)
     }catch (error) {
         res.status(500).send(error)
         console.log("erro ao enviar os dados!")
     }
 }
 export default AllBoocksCodes
+
+
+
+//async eh so pra vc declarar que aquela funcao vai ser assincrona
+//o pulo do gato esta em olhar o await, entao estamos falando ali em cima:
+// *espera a consulta ao banco de dados ser feita... e dps executa datacodes.findAll() 
