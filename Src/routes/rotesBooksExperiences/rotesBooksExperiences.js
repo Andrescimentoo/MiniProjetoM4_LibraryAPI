0a9d81e2c25b2d2bb6ecf1ad0f491e6369fec13f
Andre Nascimento <1914andre@gmail.com>
2024-12-02 11:01:04 -0300

    feat: adding routes to the CRUD in  file routesBooksExperiences

--- Src/routes/rotesBooksExperiences/rotesBooksExperiences.js
+++ Src/routes/rotesBooksExperiences/rotesBooksExperiences.js
@@ -1,8 +1,14 @@
 import { Router } from "express";
-import booksExperiences from "../../controllers/booksExperiences/booksExperiences.js";
+import booksExperiences from "../../controllers/booksExperiences/getBooksExperiences.js";
+import postExperiences from "../../controllers/booksExperiences/postExperiences.js";
+import putExperiences from "../../controllers/booksExperiences/putExperiences.js";
+import deleteExperiences from "../../controllers/booksExperiences/deleteExperiences.js";
 
 const routeBooksExperiences = Router()
 
-routeBooksExperiences.get("/routeExperiences",booksExperiences)
+routeBooksExperiences.get("/routeExperiences", booksExperiences)
+routeBooksExperiences.post("/routePostExperiences", postExperiences)
+routeBooksExperiences.put("/routePutExperiences", putExperiences)
+routeBooksExperiences.delete("/routeDeleteExperiences", deleteExperiences)
 
 export{routeBooksExperiences}
