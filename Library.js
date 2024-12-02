dd14873558b4fc0515ee0547f75cb7a96f13b45a
Andre Nascimento <1914andre@gmail.com>
2024-12-02 11:06:50 -0300

    fix: editing names tho the importations and adding one exportation in library/express

--- Library.js
+++ Library.js
@@ -1,7 +1,7 @@
 import express from "express"
 import { routeBooksCodes } from "./Src/routes/rotesBooksCodes/rotesBooksCode.js"
 import { routeBooksExperiences } from "./Src/routes/rotesBooksExperiences/rotesBooksExperiences.js"
-import {roteBooksGuides,} from "./Src/routes/rotesBooksGuides/rotesBooksGuides.js"
+import {routeBooksGuides,} from "./Src/routes/rotesBooksGuides/rotesBooksGuides.js"
 
 import cors from "cors"
 
@@ -9,7 +9,7 @@ const library = express()
 
 library.use(cors({origin: 'http://localhost:5173'}))
 library.use(express.json())
-library.use(routeBooksCodes, routeBooksExperiences, roteBooksGuides)
+library.use(routeBooksCodes, routeBooksExperiences, routeBooksGuides)
 
 
 const PORT = 5000
@@ -22,3 +22,5 @@ try {
 } catch (error) {
     console.log('Erro ao rodar aplicação!', error);
 }
+
+export default library
