ae5f81ba799ae4a80802eeeb05d77e891ef2684b
Andre Nascimento <1914andre@gmail.com>
2024-12-02 11:15:29 -0300

    feat: adding in project logic to the controller putGuides

---
+++ Src/controllers/booksGuides/putGuides.js
@@ -0,0 +1,13 @@
+import { dataGuides } from "../../models/model.js";
+
+const putGuides = async (req,res) => {
+    try {
+        const getId = await dataGuides.findByPk(req.params.id) // tipo assim nesse banco de dados captura o id dps injeta nele o metodo update 
+        const updateGuides = await getId.update(req.body)
+
+        res.status(200).send(updateGuides)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+}
+export default putGuides
