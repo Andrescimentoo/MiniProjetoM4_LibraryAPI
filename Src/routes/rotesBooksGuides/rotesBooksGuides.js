56547abeb5b8533fab196ab8f85eb6e951afa9d2
Andre Nascimento <1914andre@gmail.com>
2024-12-02 10:59:44 -0300

    feat: adding all routes to the CRUD in  file routeBooksGuides

--- Src/routes/rotesBooksGuides/rotesBooksGuides.js
+++ Src/routes/rotesBooksGuides/rotesBooksGuides.js
@@ -1,8 +1,14 @@
 import { Router } from "express";
-import booksGuides from "../../controllers/booksGuides/booksGuides.js";
+import booksGuides from "../../controllers/booksGuides/getBooksGuides.js";
+import postExperiences from "../../controllers/booksExperiences/postExperiences.js";
+import putGuides from "../../controllers/booksGuides/putGuides.js";
+import deleteGuides from "../../controllers/booksGuides/deleteGuides.js";
 
-const roteBooksGuides = Router()
+const routeBooksGuides = Router()
 
-roteBooksGuides.get("/routesGuides",booksGuides)    
+routeBooksGuides.get("/routeGetGuides",booksGuides)  
+routeBooksGuides.post("routePostGuides",postExperiences)
+routeBooksGuides.put("/routePutGuides",putGuides)
+routeBooksGuides.delete("/routeDeleteGuides",deleteGuides)  
 
-export {roteBooksGuides}               
+export {routeBooksGuides}               
