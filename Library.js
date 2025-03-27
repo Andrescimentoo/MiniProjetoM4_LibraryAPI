de8709f1cdec49bd0616dd2369e45bd2eb2dd5ad
Andre Nascimento <1914andre@gmail.com>
2025-03-27 19:17:50 -0300

    feat: debugar: importations update

--- Library.js
+++ Library.js
@@ -1,7 +1,7 @@
 import express from "express"
-import { routeBooksCodes } from "./Src/routes/rotesBooksCodes/rotesBooksCode.js"
-import { routeBooksExperiences } from "./Src/routes/rotesBooksExperiences/rotesBooksExperiences.js"
-import {routeBooksGuides,} from "./Src/routes/rotesBooksGuides/rotesBooksGuides.js"
+import { routeBooksCodes} from "./Src/routes/rotesBooksCodes/rotesBooksCode.js"
+import { routeBooksExperiences} from "./Src/routes/rotesBooksExperiences/rotesBooksExperiences.js"
+import {routeBooksGuides} from "./Src/routes/rotesBooksGuides/rotesBooksGuides.js"
 
 import cors from "cors"
 
@@ -12,12 +12,12 @@ library.use(express.json())
 library.use(routeBooksCodes, routeBooksExperiences, routeBooksGuides)
 
 
-const PORT = 5000
+const PORT = 5000 
 
 
 try {
     library.listen(PORT, () => {
-        console.log(`Aplicação está rodando em: http://localhost:${PORT}!`);
+        console.log(`Aplicação está rodando em: https://localhost:${PORT}!`);
     });
 } catch (error) {
     console.log('Erro ao rodar aplicação!', error);
